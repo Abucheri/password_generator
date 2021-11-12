632d98c0f188a48934e03c5d29d5679d3ac80ee9
O'Brien <abucheriobrien@gmail.com>
2021-11-12 17:00:10 +0300

    final changes

--- final_copy.py
+++ final_copy.py
@@ -4,7 +4,7 @@ print('Eunique password generator')
 
 length = 15 # length of the password(15)
 
-custom = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-[]}{`=+"><?,.:;/\'|'  # custom parameters with numbers, letters and symbols
+custom = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-[]}{`=+"><?,.:;/\'\|'  # custom parameters with numbers, letters and symbols
 
 ran = random.sample(custom, length)  # using the randon library to generate random characters
 
